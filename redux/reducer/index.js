ecba04b795e809a5ca57b6413bfc3f1b5b0b6192
MichelaKalowski <mi.ky90@hotmail.com>
2023-02-16 17:12:58 +0100

    aggiornamento

---
+++ redux/reducer/index.js
@@ -0,0 +1,15 @@
+const InitialState={
+
+    job:{
+        content:[]
+    }
+
+}
+
+const mainReducer=(state=InitialState, action)=>{
+    switch(action.type){
+default:
+    return state
+    }
+}
+export default mainReducer;
